14e418aa63ac13855224eb25c16126882793f4d9
Alexander Kudryk <alexander.kudryk@thestreet.com>
2018-01-06 21:59:00 -0500

    using yargs alone

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
-console.log('Process: ', process.argv);
 console.log('Yargs: ', argv);
 
 switch (command) {
